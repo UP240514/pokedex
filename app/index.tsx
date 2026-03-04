3dd19bc43420d1458e380f0cde605714cbba181e
UP240514 <up240514@alumnos.upa.edu.mx>
2026-03-04 14:58:19 -0600

    show pokemons on screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,32 +1,35 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Text, View } from "react-native";
 
 export default function Index() {
+  const [results, setResults] = useState<any[]>([]);
   useEffect(() => {
-    console.log("entre en pantalla");
+    console.log("Entre en pantalla");
     getPokemons();
   }, []);
 
   const getPokemons = async () => {
     try {
-      const URL = "https://poke/api/v2/pokemon?limit=100000&offset=0";
+      const URL = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0";
       const response = await fetch(URL, {
         method: "GET",
       });
-
       if (response.ok) {
-        console.log("Respuesta ok");
+        const data = await response.json();
+        setResults(data.results);
       } else {
-        console.log("bard request");
+        console.log("Bard Request");
       }
     } catch (error) {
-      console.log("ocurrio un error");
+      console.log("Ocurrió un error");
     }
   };
 
   return (
     <View>
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      {results.map((item) => {
+        return <Text>{item.name}</Text>;
+      })}
     </View>
   );
 }
